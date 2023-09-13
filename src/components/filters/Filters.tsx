06ffabcf381c5b2d24e4a17890e154122e694343
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-09-13 17:15:18 -0300

    Pr para revisão

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -1,5 +1,5 @@
 // import React, { useState } from 'react';
-import InputText from '../inputText/InputText';
+// import InputText from '../inputText/InputText';
 import useFilter from '../../hooks/useFilter';
 
 // const INITIAL_STATE = {
@@ -9,7 +9,7 @@ import useFilter from '../../hooks/useFilter';
 export default function Filters() {
   // const [filters, setFilters] = useState(INITIAL_STATE);
 
-  const [filters, handleChange] = useFilter();
+  const [filters, handleChange, filterPlanetValues] = useFilter();
 
   // const handleChange = (
   //   { target: { name, value } }:React.ChangeEvent<HTMLInputElement>,
@@ -23,22 +23,70 @@ export default function Filters() {
 
   return (
     <section>
-      <form>
+      <form
+        onSubmit={ (event) => {
+          event.preventDefault();
+          filterPlanetValues();
+        } }
+      >
 
-        {/* <input
-          onChange={ handleChange }
-          placeholder="filter by name"
-          type="text"
-          value={ filters.name }
-        /> */}
-        <InputText
+        <div>
+          <input
+            data-testid="name-filter"
+            onChange={ handleChange }
+            placeholder="filter by name"
+            type="text"
+            value={ filters.name }
+            name="name"
+          />
+        </div>
+
+        <div>
+          <select
+            onChange={ handleChange }
+            data-testid="column-filter"
+            name="column"
+            value={ filters.column }
+          >
+            <option value="population">Population</option>
+            <option value="orbital_period">Orbital period</option>
+            <option value="diameter">Diameter</option>
+            <option value="rotation_period">Rotation period</option>
+            <option value="surface_water">Surface water</option>
+          </select>
+
+          <select
+            onChange={ handleChange }
+            data-testid="comparison-filter"
+            name="comparison"
+            value={ filters.comparison }
+          >
+            <option value="maior que">Maior que</option>
+            <option value="menor que">Menor que</option>
+            <option value="igual a">Igual a</option>
+          </select>
+
+          <input
+            onChange={ handleChange }
+            data-testid="value-filter"
+            type="number"
+            name="value"
+            value={ filters.value }
+          />
+          <button
+            data-testid="button-filter"
+          >
+            Filtrar
+          </button>
+        </div>
+        {/* <InputText
           handleChange={ handleChange }
           name="name"
           label=" "
           value={ filters.name }
           placeholder="filter by name"
           testId="name-filter"
-        />
+        /> */}
       </form>
     </section>
   );
--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -4,6 +4,9 @@ import { PlanetType } from '../types/types';
 
 const INITIAL_STATE = {
   name: '',
+  column: 'population',
+  comparison: 'maior que',
+  value: '0',
 };
 
 const useFilter = () => {
@@ -23,6 +26,12 @@ const useFilter = () => {
     });
   };
 
+  // const comparation = () => {
+  //   if (filters.comparison === 'maior que') {
+
+  //   }
+  // };
+
   const filterPlanetName = () => {
     const newPlanetsList:PlanetType[] = planets
       .filter((planet) => planet.name
@@ -30,12 +39,46 @@ const useFilter = () => {
     setplanetsFiltered(newPlanetsList);
   };
 
+  const operationChosenFilter = (planet:any) => {
+    switch (filters.comparison) {
+      case 'maior que':
+        return planet[filters.column] > filters.value;
+      case 'menor que':
+        return planet[filters.column] < filters.value;
+      case 'igual a':
+        return planet[filters.column] === filters.value;
+      default:
+        break;
+    }
+
+    // if (filters.comparison === 'maior que') {
+    //   return filters.column > filters.value;
+    // }
+    // if (filters.comparison === 'menor que') {
+    //   return filters.column < filters.value;
+    // } if (filters.comparison === 'igual a') {
+    //   return filters.column === filters.value;
+    // }
+  };
+
+  const filterPlanetValues = () => {
+    const newPlanetsList:PlanetType[] = planets
+      .filter((planet) => operationChosenFilter(planet));
+
+    setplanetsFiltered(newPlanetsList);
+    // setFilters(INITIAL_STATE);
+
+    // console.log('clicou');
+    // console.log(newPlanetsList);
+  };
+
   useEffect(() => {
     filterPlanetName();
-    console.log('effect useFilter');
+    // console.log('effect useFilter');
+    // console.log(filters);
   }, [filters]);
 
-  return [filters, handleChange];
+  return [filters, handleChange, filterPlanetValues];
 };
 
 export default useFilter;
